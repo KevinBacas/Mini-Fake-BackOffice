026f941afeb613310f74e83143e46b0347bd0351
kevinbacas <bacas.kevin@hotmail.fr>
2015-01-05 10:31:04 +0100

    First actions and Globals

--- CBackOfficeView.php
+++ CBackOfficeView.php
@@ -8,6 +8,8 @@
  * @copyright APLICAEN
  */
 
+require_once('Globals.php');
+
 class CBackOfficeView {
 
   private $m_XMLManager;
@@ -21,6 +23,8 @@ class CBackOfficeView {
     <!DOCTYPE html>
     <head>
       <title>Back Office</title>
+      <meta charset='utf-8'/>
+
       <link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' rel='stylesheet'>
     </head>
     <body>
@@ -28,13 +32,44 @@ class CBackOfficeView {
     ";
   }
 
+  private function getTopBar(){
+    $res = "
+      <!-- Static navbar -->
+      <nav class='navbar navbar-default'>
+      <div class='container-fluid'>
+      <div class='navbar-header'>
+      <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
+      <span class='sr-only'>Toggle navigation</span>
+      <span class='icon-bar'></span>
+      <span class='icon-bar'></span>
+      <span class='icon-bar'></span>
+      </button>
+      <a class='navbar-brand' href='#'>Back Office</a>
+      </div>
+      <div id='navbar' class='navbar-collapse collapse'>
+      </div><!--/.nav-collapse -->
+      </div><!--/.container-fluid -->
+      </nav>
+    ";
+    return $res;
+  }
+
   private function getFooter(){
     return "
-      </div>
-      <script src='http://code.jquery.com/jquery.min.js'></script>
-      <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
-    </body>
-    </html>
+          <hr>
+          <div class='row'>
+            <div class='col-lg-12'>
+              <ul class='nav nav-pills nav-justified'>
+              <li><a href='http://www.aplicaen.fr'>© 2015 APLICAEN, Élèves-entrepreneurs</a></li>
+              <li><a href='http://www.expansion2plus1.fr/'>© 2015 Expansion2plus1</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <script src='http://code.jquery.com/jquery.min.js'></script>
+        <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
+      </body>
+      </html>
     ";
   }
 
@@ -57,12 +92,14 @@ class CBackOfficeView {
       foreach($fields as $field){
         $name = $field->getId();
         $content = $field->getContent();
+        $edit_action_name = EDIT_ACTION_NAME;
+        $fieldname_get = FIELDNAME_GET;
         $res .= "
           <tr>
             <td>$name</td>
             <td>$content</td>
             <td>
-              <a href='#' class='btn btn-primary'>
+              <a href='./index.php?action=$edit_action_name&$fieldname_get=$name' class='btn btn-primary'>
                 Editer
               </a>
               <a href='#' class='btn btn-danger'>
@@ -193,8 +230,31 @@ class CBackOfficeView {
     return $res;
   }
 
+  public function authenticationFormBootstrap($error){
+    $res = "
+      <div class='row'>
+        <div class='col-md-offset-4 col-md-4'>
+          <div class='form-login'>
+            <h4>Veuillez vous identifier.</h4>
+            <form action='index.php?action=connect' method='GET' class='form-horizontal'>
+              <input type='text' id='username' name='username' class='form-control input-sm chat-input' placeholder='username' />
+              <input type='text' id='password' name='password' class='form-control input-sm chat-input' placeholder='password' />
+              <div class='control-group'>
+                <button type='submit' class='btn btn-primary'>Se connecter</button>
+              </div>
+              <input type='text' name='action' value='connect' hidden/>
+            </form>
+            <div class='alert alert-danger' role='alert'>$error</div>
+          </div>
+        </div>
+      </div>
+    ";
+    return $res;
+  }
+
   public function listAllFieldsView(){
     $res = $this->getHeader();
+    $res .= $this->getTopBar();
     $res .= $this->listFieldsAsBoostrapTab();
     $res .= $this->getFooter();
     return $res;
@@ -202,6 +262,7 @@ class CBackOfficeView {
 
   public function editFieldView($field){
     $res = $this->getHeader();
+    $res .= $this->getTopBar();
     $res .= $this->editFieldBootstrap($field);
     $res .= $this->getFooter();
     return $res;
@@ -209,6 +270,7 @@ class CBackOfficeView {
 
   public function listAllUsersView(){
     $res = $this->getHeader();
+    $res .= $this->getTopBar();
     $res .= $this->listUsersAsBoostrapTab();
     $res .= $this->getFooter();
     return $res;
@@ -216,6 +278,7 @@ class CBackOfficeView {
 
   public function createUserView(){
     $res = $this->getHeader();
+    $res .= $this->getTopBar();
     $res .= $this->createUserBootstrap();
     $res .= $this->getFooter();
     return $res;
@@ -223,7 +286,16 @@ class CBackOfficeView {
 
   public function editUserView($user){
     $res = $this->getHeader();
+    $res .= $this->getTopBar();
     $res .= $this->editUserBootstrap($user);
+    $res .= $this->getFooter();
+    return $res;
+  }
+
+  public function authenticationView($error = ''){
+    $res = $this->getHeader();
+    $res .= $this->getTopBar();
+    $res .= $this->authenticationFormBootstrap($error);
     $res .= $this->getFooter();
     return $res;
   }
--- index.php
+++ index.php
@@ -1,95 +1,135 @@
 <?php
 
   /**
-   * Work as a testing field.
-   * Will be replaced by full engine as soon as code will be ready
+   * Main operator of the code.
    *
    * @author Kévin BACAS
    * @copyright APLICAEN
    */
 
+  // !!! REQUIRES
+  require_once('Globals.php'); // Had to be first
   require_once('CUserModel.php');
   require_once('CFieldModel.php');
   require_once('CXMLManager.php');
   require_once('CBackOfficeView.php');
   require_once('CAuthManager.php');
 
-  echo "<meta charset='utf-8'/>";
+  // !!! GETTING CONTROLLER PARAMETERS
+  $action = @$_GET[ACTION_GET];
+  $field_name = @$_GET[FIELDNAME_GET];
+  $connection_error = @$_GET[CONNECTION_ERROR_GET];
+  $username = @$_GET[USERNAME_GET];
+  $password = @$_GET[PASSWORD_GET];
 
-  echo "Testing UserModel...<hr>";
-  $user = new CUserModel("Kevbac", "123456");
-  $user_serialized = serialize($user);
-  $user_unserialized = unserialize($user_serialized);
+  // !!! CONTROLLER
+  if(!$action || $action == AUTH_ACTION_NAME){
 
-  echo $user_serialized;
-  var_dump($user_unserialized);
+    $xml_manager = new CXMLManager(XML_PRODUCTION_FILENAME);
+    $back_office_view = new CBackOfficeView($xml_manager);
+    $auth_manager = new CAuthManager($xml_manager);
 
-  echo "Testing FieldModel...<hr>";
-  $field = new CFieldModel("HomePageMainText", "Lorem ipsum tout ça...");
-  $field_serialized = serialize($field);
-  $field_unserialized = unserialize($field_serialized);
+    if($auth_manager->isConnected()){
+      echo $back_office_view->listAllFieldsView();
+    } else {
+      echo $back_office_view->authenticationView(($connection_error ? $connection_error : ''));
+    }
 
-  echo $field_serialized;
-  var_dump($field_unserialized);
+  } else if($action == CONNECT_ACTION_NAME) {
 
-  echo "Testing XMLManager...<hr>";
-  $file_name = "qq.xml";
-  $xml_manager = new CXMLManager($file_name);
-  if(file_exists($file_name)){
+    $xml_manager = new CXMLManager(XML_PRODUCTION_FILENAME);
+    $auth_manager = new CAuthManager($xml_manager);
+    $auth_manager->connect($username, $password);
+    if($auth_manager->isConnected()){
+      header('Location: index.php');
+    } else {
+      header('Location: index.php?connection_error="Nom d\'utilisateur/Mot de passe invalide(s)"');
+    }
+
+  } else if($action == GET_FIELD_ACTION_NAME) {
+
+    $xml_manager = new CXMLManager(XML_PRODUCTION_FILENAME);
+    $field = $xml_manager->getField($field_name);
+    $field_content = ($field ? $field->getContent() : '');
+    echo json_encode(array('fieldcontent' => $field_content));
+
+  } else if($action == TEST_ACTION_NAME) {
+    echo "<meta charset='utf-8'/>";
+
+    echo "Testing UserModel...<hr>";
+    $user = new CUserModel("Kevbac", "123456");
+    $user_serialized = serialize($user);
+    $user_unserialized = unserialize($user_serialized);
+
+    echo $user_serialized;
+    var_dump($user_unserialized);
+
+    echo "Testing FieldModel...<hr>";
+    $field = new CFieldModel("HomePageMainText", "Lorem ipsum tout ça...");
+    $field_serialized = serialize($field);
+    $field_unserialized = unserialize($field_serialized);
+
+    echo $field_serialized;
+    var_dump($field_unserialized);
+
+    echo "Testing XMLManager...<hr>";
+    $xml_manager = new CXMLManager(XML_TEST_FILENAME);
+    if(file_exists($xml_test_filename)){
+      echo htmlentities($xml_manager->toString()) . "<br/>";
+    }else{
+      echo "FAIL !";
+    }
+
+    echo "Testing register field...<hr>";
+    $field_name = "qq";
+    $new_field = new CFieldModel($field_name, "lel");
+    $xml_manager->registerField($new_field);
     echo htmlentities($xml_manager->toString()) . "<br/>";
-  }else{
-    echo "FAIL !";
+
+    $field_name2 = "azerty";
+    $new_field2 = new CFieldModel($field_name2, "lel");
+    $xml_manager->registerField($new_field2);
+    echo htmlentities($xml_manager->toString()) . "<br/>";
+
+    $field = $xml_manager->getField($field_name);
+    var_dump($field);
+    $field->setContent("QQV2 LEL");
+    $xml_manager->updateField($field);
+    $field = $xml_manager->getField($field_name);
+    var_dump($field);
+
+    $back_office_view = new CBackOfficeView($xml_manager);
+    var_dump($xml_manager->getAllFields());
+    echo $back_office_view->listAllFieldsView();
+    echo $back_office_view->editFieldView($field);
+
+    echo "Testing user creation...<hr>";
+    $new_user_username = 'kevbac';
+    $new_user_password = 'azerty';
+    $new_user = new CUserModel($new_user_username, $new_user_password);
+    $xml_manager->createUser($new_user);
+    var_dump($xml_manager->readUser($new_user_username));
+    $new_user_password = $new_user_password . 'lel';
+    $new_user->updatePassword($new_user_password);
+    $xml_manager->updateUser($new_user);
+    var_dump($xml_manager->readUser($new_user_username));
+    echo htmlentities($xml_manager->toString()) . "<br/>";
+
+    echo "Testing authentication...<hr>";
+    $auth_manager = new CAuthManager($xml_manager);
+    $auth_manager->connect($new_user_username, $new_user_password);
+    echo 'Is connected ? ' . ($auth_manager->isConnected() ? "Yes" : "No") . '<br/>';
+    $auth_manager->disconnect();
+    echo 'Is connected ? ' . ($auth_manager->isConnected() ? "Yes" : "No") . '<br/>';
+
+    echo $back_office_view->listAllUsersView();
+    echo $back_office_view->createUserView();
+    echo $back_office_view->editUserView($new_user);
+
+    $xml_manager->deleteUser($new_user);
+    var_dump($xml_manager->readUser($new_user_username));
+
+    unlink($xml_test_filename);
   }
-
-  echo "Testing register field...<hr>";
-  $field_name = "qq";
-  $new_field = new CFieldModel($field_name, "lel");
-  $xml_manager->registerField($new_field);
-  echo htmlentities($xml_manager->toString()) . "<br/>";
-
-  $field_name2 = "azerty";
-  $new_field2 = new CFieldModel($field_name2, "lel");
-  $xml_manager->registerField($new_field2);
-  echo htmlentities($xml_manager->toString()) . "<br/>";
-
-  $field = $xml_manager->getField($field_name);
-  var_dump($field);
-  $field->setContent("QQV2 LEL");
-  $xml_manager->updateField($field);
-  $field = $xml_manager->getField($field_name);
-  var_dump($field);
-
-  $back_office_view = new CBackOfficeView($xml_manager);
-  var_dump($xml_manager->getAllFields());
-  echo $back_office_view->listAllFieldsView();
-  echo $back_office_view->editFieldView($field);
-
-  echo "Testing user creation...<hr>";
-  $new_user_username = 'kevbac';
-  $new_user_password = 'azerty';
-  $new_user = new CUserModel($new_user_username, $new_user_password);
-  $xml_manager->createUser($new_user);
-  var_dump($xml_manager->readUser($new_user_username));
-  $new_user_password = $new_user_password . 'lel';
-  $new_user->updatePassword($new_user_password);
-  $xml_manager->updateUser($new_user);
-  var_dump($xml_manager->readUser($new_user_username));
-  echo htmlentities($xml_manager->toString()) . "<br/>";
-
-  echo "Testing authentication...<hr>";
-  $auth_manager = new CAuthManager($xml_manager);
-  $auth_manager->connect($new_user_username, $new_user_password);
-  echo 'Is connected ? ' . ($auth_manager->isConnected() ? "Yes" : "No") . '<br/>';
-  $auth_manager->disconnect();
-  echo 'Is connected ? ' . ($auth_manager->isConnected() ? "Yes" : "No") . '<br/>';
-
-  echo $back_office_view->listAllUsersView();
-  echo $back_office_view->createUserView();
-  echo $back_office_view->editUserView($new_user);
-
-  $xml_manager->deleteUser($new_user);
-  var_dump($xml_manager->readUser($new_user_username));
-
-  unlink($file_name);
 
 ?>
